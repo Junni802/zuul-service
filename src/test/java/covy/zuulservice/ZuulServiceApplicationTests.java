f482780171f24513270f81d33c8a7b0c4c8117c4
박준혁/PD프로젝트 <junni802@gsretail.com>
2025-02-12 16:03:18 +0900

    feat: add zuul-service project

---
+++ src/test/java/covy/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
